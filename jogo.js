5fea2f74c0548cc9a51bb80b9f9c45cd00380701
CtrlBBT <ctrlmarkbbt@gmail.com>
2024-07-24 19:13:25 -0300

    feat: adicionado criar

---
+++ jogo.js
@@ -0,0 +1,34 @@
+const prompt = require ('prompt-sync')();
+
+const jogos = []
+
+const criar = () => {
+    const nome = prompt("Qual o Nome do Jogo? ")
+    const ano_lancamento = prompt("Qual o Ano de Lançamento do Jogo? ")
+    const duracao = prompt("Qual a Duração média de Jogo? (EM HORAS) ")
+    const preco = prompt("Qual o Preço do jogo? ")
+    const estudio = prompt("Qual o Estúdio do Jogo? ")
+    const sequencia = prompt("Qual é a Sequência do Jogo? ")
+
+    if (
+        nome != "" &&
+        ano_lancamento >= 1962 && ano_lancamento <= 2024 &&
+        duracao > 0 &&
+        preco == 0 &&
+        estudio != "" &&
+        ((sequencia > 0 && sequencia < jogos.length) || jogos.length == 0)
+    ) {
+        jogos.push ({
+            nome,
+            ano_lancamento,
+            duracao,
+            preco,
+            estudio,
+            sequencia,
+        })
+        console.log("Jogo Cadastrado com Sucesso!!!")
+    } else {
+        console.log("Dados Inválidos.")
+    }    
+
+}
